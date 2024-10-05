0ac9f15c686e0d7eabe2148a30078405fc7abe68
PamelaTerrell <pammyhoney@yahoo.com>
2024-10-05 16:34:57 -0400

    WTH

--- script2.mjs
+++ script2.mjs
@@ -1,20 +1,20 @@
 document.addEventListener ("DOMContentLoaded", () => {
-    fetchInsults();})
+    fetchInsults();
+})
 
 
     function fetchInsults (){
         fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json' , {mode: 'no-cors'})
         .then(resp => resp.json())
-        .then(json =>
-        renderInsult(json.data)
+        .then(json => renderInsult(json.data)
         )}
 
     
 
-    function renderInsult (insults) {
+    function renderInsult(insults) {
         let insultMe = document.getElementById("insult-text")
-        facts.map(insult =>{
-        const p = document.createElement ('p')
+        insults.map(insult =>{
+        const p = document.createElement('p')
         p.setAttribute("id", "actual-insult")
         p.innerHTML = insult.insult
         insultMe.appendChild(p)})
