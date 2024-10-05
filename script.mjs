19ca2091fd5837165b12720a061ca651fff49a54
PamelaTerrell <pammyhoney@yahoo.com>
2024-10-04 21:32:21 -0400

    I am a failure

--- script.mjs
+++ script.mjs
@@ -3,14 +3,21 @@ fetchData();
 async function fetchData(){
 
     try{
+
+        
         const response = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json");
+
+        response = await response.json();
 
         if(!response.ok){
             throw new Error("could not fetch");
         }
+            
+        
+        fetchData();
 
-        const data = await response.json();
-        console.log(data);
+        response.json();
+        
 
 
     }
