935d66ad0cad6a74e7c767f471b5c3972c791fcb
PamelaTerrell <pammyhoney@yahoo.com>
2024-10-06 13:21:57 -0400

    stillNoWorkee

---
+++ README.md
@@ -0,0 +1,11 @@
+Project title - Insult Me
+
+This website generates insults by accesing API 
+https://cors-anywhere.herokuapp.com/"; const targetUrl =
+https://evilinsult.com/generate_insult.php?lang=en&type=json
+
+References -
+
+https://youtu.be/lfmg-EJ8gm4?si=j6lkNx8byeCwd8K1
+
+jpg from Unsplash.com
--- script2.mjs
+++ script2.mjs
@@ -1,21 +1,20 @@
-document.addEventListener ("DOMContentLoaded", () => {
-    fetchInsults();
-})
-
-
-    function fetchInsults (){
-        fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json' , {mode: 'no-cors'})
-        .then(resp => resp.json())
-        .then(json => renderInsult(json.data)
-        )}
-
-    
-
-    function renderInsult(insults) {
-        let insultMe = document.getElementById("insult-text")
-        insults.map(insult =>{
-        const p = document.createElement('p')
-        p.setAttribute("id", "actual-insult")
-        p.innerHTML = insult.insult
-        insultMe.appendChild(p)})
-    }
+// Proxy url is needed to circumvent the cors issue.
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"; const targetUrl =
+  "https://evilinsult.com/generate_insult.php?lang=en&type=json";
+document.addEventListener("DOMContentLoaded", () => { fetchInsults();
+});
+async function fetchInsults() { try {
+const response = await fetch(proxyUrl + targetUrl,
+{ cache: "no-store" }); // this part allows you to get a different insult with each clic
+const insultData = await response.json();
+    renderInsult(insultData);
+  } catch (error) {
+console.log(error); }
+}
+function renderInsult(insult) {
+let insultMe = document.getElementById("insult-text");
+  // Since `insult` is a single object, you don't need to map over it
+const p = document.createElement("p");
+p.setAttribute("id", "actual-insult");
+p.innerHTML = insult.insult; // Directly access `insult.insult` insultMe.appendChild(p);
+}
